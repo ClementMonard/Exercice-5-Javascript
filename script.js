128ef42f1760b91cc201cd413eccb3d9afa5a896
ClementMonard <clement.monard@hotmail.fr>
2018-06-20 18:39:23 +0200

    Corrections majeurs + ajout du regex

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function nombre(){
-  var firstNumber = document.getElementById('firstNumber').value;
-  var secondNumber = document.getElementById('secondNumber').value;
-
-  var result = firstNumber * secondNumber;
-  alert(result);
+function calculMultiplication(){
+var firstNumber = document.getElementById("firstNumber").value;
+var secondNumber = document.getElementById("secondNumber").value;
+  if(/^[0-9.]+$/.test(firstNumber && secondNumber) == true){
+    firstNumber = Math.floor(firstNumber);
+    secondNumber = Math.floor(secondNumber);
+    var result = firstNumber * secondNumber;
+    alert(result);
+  }else{
+    alert('Ce n\'est pas un nombre.');
+  }
 }
